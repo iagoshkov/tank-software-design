1a7c6d0edfc9f0a2c921c3c0d829c9006a02ab91
chesnokoff <chesnokoff239@gmail.com>
2025-10-12 23:28:53 +0300

    Update KeyboardController: extract keyMap to constructor arguments

--- src/main/java/ru/mipt/bit/platformer/controller/KeyboardController.java
+++ src/main/java/ru/mipt/bit/platformer/controller/KeyboardController.java
@@ -16,7 +16,14 @@ public class KeyboardController implements InputController {
     private static final GameLogger logger = GameLogger.getLogger(KeyboardController.class);
 
     /** Key map. */
-    private static final Map<Integer, Direction> keyMap = Map.of(
+    private final Map<Integer, Direction> keyMap;
+
+    public KeyboardController(Map<Integer, Direction> keyMap) {
+        this.keyMap = keyMap;
+    }
+
+    public KeyboardController() {
+        this(Map.of(
             Input.Keys.W, Direction.UP,
             Input.Keys.UP, Direction.UP,
             Input.Keys.S, Direction.DOWN,
@@ -25,7 +32,8 @@ public class KeyboardController implements InputController {
             Input.Keys.LEFT, Direction.LEFT,
             Input.Keys.D, Direction.RIGHT,
             Input.Keys.RIGHT, Direction.RIGHT
-    );
+        ));
+    }
 
     /** {@inheritDoc} */
     @Override
